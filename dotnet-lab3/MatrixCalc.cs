649f82ebfe9331ffb964619a2a8a9e565f834f5f
Aleksander Wolski <275444@student.pwr.edu.pl>
2025-04-10 23:14:27 +0200

    Dodaj pliki projektów.

---
+++ dotnet-lab3/MatrixCalc.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet_lab3
+{
+    class MatrixCalc
+    {
+        public int[,] A { get; set; }
+        public int[,] B { get; set; }
+        public int[,] Result { get; set; }
+        int NThreads = 1;
+        int RngSeed = 0;
+        Random rand;
+        int MinCellValue = 0;
+        int MaxCellValue = 1000;
+        public MatrixCalc(int NThreads, int MatrixSize)
+        {
+            this.NThreads = NThreads;
+            RngSeed = DateTime.Now.Millisecond;
+            this.rand = new Random(RngSeed);
+
+            A = new int[MatrixSize, MatrixSize];
+            B = new int[MatrixSize, MatrixSize];
+            Result = new int[A.GetLength(0), B.GetLength(1)];
+        }
+
+        public void SetRandomValues()
+        {
+            for (int m = 0; m < 2; m++)
+            {
+                int[,] Matrix = m == 0 ? A : B;
+                for (int i = 0; i < Matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < Matrix.GetLength(1); j++)
+                    {
+                        Matrix[i, j] = rand.Next(MinCellValue, MaxCellValue + 1);
+                    }
+                }
+            }
+        }
+
+        public void MultiplyCell(int n, int m)
+        {
+            int Sum = 0;
+            for (int i = 0; i < A.GetLength(1); i++) 
+            {
+                Sum += A[n, i] * B[i, m];
+            }
+            Result[n, m] = Sum; 
+        }
+
+
+        public void PrintMatrix(int[,] Matrix)
+        {
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < Matrix.GetLength(1); j++)
+                {
+                    Console.Write(Matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+}
---
+++ dotnet-lab3/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace dotnet_lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 500;
+            MatrixCalc matrixCalc = new MatrixCalc(4, size);
+            matrixCalc.SetRandomValues();
+
+            //Console.WriteLine("Matrix A:");
+            //matrixCalc.PrintMatrix(matrixCalc.A);
+
+            //Console.WriteLine("Matrix B:");
+            //matrixCalc.PrintMatrix(matrixCalc.B);
+
+            //for (int i = 0; i < size; i++)
+            //{
+            //    for (int j = 0; j < size; j++)
+            //    {
+            //        Console.WriteLine($"{i} {j} calc");
+            //        matrixCalc.MultiplyCell(i, j);
+            //    }
+            //}
+
+
+
+            for (int run = 0; run < 15; run++)
+            {
+                matrixCalc = new MatrixCalc(4, size);
+
+                matrixCalc.SetRandomValues();
+
+                ParallelOptions opt = new ParallelOptions() { MaxDegreeOfParallelism = 16 };
+                int[] ThreadsUsed = new int[30];
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                Parallel.For(0, size, opt, x =>
+                {
+                    for (int i = 0; i < size; i++)
+                    {
+                        matrixCalc.MultiplyCell(x, i);
+                    }
+                    ThreadsUsed[Thread.CurrentThread.ManagedThreadId]++;
+                });
+                watch.Stop();
+                //Console.WriteLine($"run {run} took {watch.ElapsedMilliseconds} ms.");
+                Console.WriteLine($"{watch.ElapsedMilliseconds}");
+
+
+            }
+
+
+            //matrixCalc.PrintMatrix(matrixCalc.Result);
+        }
+    }
+}
